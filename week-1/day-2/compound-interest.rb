212e7b501b2dcbf3d2a5c486604c072f5dbb7f42
humberto delgado <dehumberto@gmail.com>
2020-04-14 16:34:33 -0500

    indent

--- week-1/day-2/compound-interest.rb
+++ week-1/day-2/compound-interest.rb
@@ -9,8 +9,8 @@ periods = months
 interest = 1 + interest/100
 
 while (periods > 0)
-amount = amount * interest
-periods = periods - 1
+    amount = amount * interest
+    periods = periods - 1
 end 
 
 puts "After #{months} months you will get #{amount.round(2)}"
