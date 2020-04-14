5417156e9437b1268cebf4a43abf2628ca33616d
humberto delgado <dehumberto@gmail.com>
2020-04-14 11:22:05 -0500

    Compound insterest assignment solution

---
+++ week-1/day-2/compound-interest.rb
@@ -0,0 +1,16 @@
+print "Enter amount: "
+amount = gets.chomp.to_f
+print "Enter interest (%): "
+interest = gets.chomp.to_f
+print "Enter months: "
+months = gets.chomp.to_i
+
+periods = months
+interest = 1 + interest/100
+
+while (periods > 0)
+amount = amount * interest
+periods = periods - 1
+end 
+
+puts "After #{months} months you will get #{amount.round(2)}"
